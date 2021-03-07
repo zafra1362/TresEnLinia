564cb23b61200af8fc531b7f777e64e190fbf1d3
Dylan <dzafrar01@elpuig.xeill.net>
2021-03-07 19:16:30 +0100

    Acabado, los botones van un poco trambolicos pero van.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,11 +2,9 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 public class Main extends Application {
